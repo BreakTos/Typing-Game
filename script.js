88c8cd24c5823dd5e4d851a75deea2e32197ad43
Parth Seth <parthseth2004@gmail.com>
2023-07-03 16:08:24 +0530

    styling of buttons

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function check(event){
     var key = event.key.toLowerCase();
     var keys = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
             'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
-            'z', 'x', 'c', 'v', 'b', 'n', 'm', ' '];
+            'z', 'x', 'c', 'v', 'b', 'n', 'm', ',','.',' '];
 
     keys.forEach(ch => {
         if(ch==key) addColor(buttons[keys.indexOf(ch)]);
